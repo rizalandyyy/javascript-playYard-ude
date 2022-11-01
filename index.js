b32060d12df014195f372123959ce31d14c34746
Astra Daihatsu Motor <official.apps@daihatsu.astra.co.id>
2022-11-01 22:01:52 +0700

    static method

--- index.js
+++ index.js
@@ -1,95 +1,94 @@
 function ConstructorFunc(param1, param2) {
-    this.firstName = param1;
-    this.lastName = param2;
-    this.sayHello = function() {
-        console.log("Hello " + this.firstName + " " + this.lastName);
-    }
+  this.firstName = param1;
+  this.lastName = param2;
+  this.sayHello = function () {
+    console.log("Hello " + this.firstName + " " + this.lastName);
+  };
 }
 
-const obj = new ConstructorFunc('John', 'Doe');
+const obj = new ConstructorFunc("John", "Doe");
 obj.sayHello();
 console.log(obj.firstName);
 
 class Person {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.sayHello = function(name) {
-
-        }
-    }
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.sayHello = function (name) {};
+  }
 }
 const eko = new Person();
 
 class Employee {
-    constructor(name) {
-        this.name = name;
-    }
+  constructor(name) {
+    this.name = name;
+  }
 
-    sayHello(name) {
-        console.info(`Hello ${name}`);
-    }
+  sayHello(name) {
+    console.info(`Hello ${name}`);
+  }
 }
 
 class Manager extends Employee {
-    constructor(name) {
-        this.name = name;
-    }
+  constructor(name) {
+    super();
+    this.name = name;
+  }
 
-    sayHello(name) {
-        console.info(`Hello ${name}`);
-    }
+  sayHello(name) {
+    console.info(`Hello ${name}`);
+  }
 }
 
 class Shape {
-    paint() {
-        console.log('paint');
-    }
+  paint() {
+    console.log("paint");
+  }
 }
 
 class Circle extends Shape {
-    paint() {
-        console.log('paint circle');
-    }
+  paint() {
+    console.log("paint circle");
+  }
 }
 
 const circle = new Circle();
 circle.paint();
 
 class getterSetter {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-    get fullName() {
-        return `${this.firstName} ${this.lastName}`;
-    }
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 
-    set fullName(value) {
-        const parts = value.split(' ');
-        this.firstName = parts[0];
-        this.lastName = parts[1];
-    }
+  set fullName(value) {
+    const parts = value.split(" ");
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  }
 }
 
 const eko123 = new Person("Eko", "Wibowo");
-console.log(eko.fullName);
+// console.log(eko.fullName);
 
 class Customer {
-    firstName;
-    lastName;
-    balance = 0;
+  firstName;
+  lastName;
+  balance = 0;
 
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-    sayHello() {
-
-    }
+  sayHello() {}
 }
 
 const rizal = new Customer("Eko", "Wibowo");
-console.info(rizal)
+console.info(rizal);
+
+// Static Method
